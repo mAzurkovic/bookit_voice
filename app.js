c58d3fcdc3deee128b4e8cde1d0394b08cdfbcab
Mattias Zurkovic <mattias@zurkovic.com>
2019-01-27 11:21:38 -0800

    add time out

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const {dialogflow} = require('actions-on-google')
 const mongoose = require('mongoose');
+var dateFormat = require('dateformat');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
