871c415eecb4a611db0a43769b0c73de0d23a12f
Mattias Zurkovic <mattias@zurkovic.com>
2019-01-27 02:25:24 -0800

    add rich text format

--- app.js
+++ app.js
@@ -60,9 +60,49 @@ app.post('/fulfillment', function (req, res) {
       } else {
           if (doc.isReserved == 1) {
             let responseObj = {
-              "fulfillmentText": "Sorry, all rooms are booked",
-              "fulfillmentMessages": [{"text":{"text": ["Sorry, all rooms are booked"]}}]
-            }
+    "conversationToken": "",
+    "expectUserResponse": true,
+    "expectedInputs": [
+        {
+            "inputPrompt": {
+                "richInitialPrompt": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": "Math and prime numbers it is!"
+                            }
+                        },
+                        {
+                            "basicCard": {
+                                "title": "Math & prime numbers",
+                                "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                                "image": {
+                                    "url": "https://example.google.com/42.png",
+                                    "accessibilityText": "Image alternate text"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Read more",
+                                        "openUrlAction": {
+                                            "url": "https://example.google.com/mathandprimes"
+                                        }
+                                    }
+                                ],
+                                "imageDisplayOptions": "CROPPED"
+                            }
+                        }
+                    ],
+                    "suggestions": []
+                }
+            },
+            "possibleIntents": [
+                {
+                    "intent": "actions.intent.TEXT"
+                }
+            ]
+        }
+    ]
+}
             res.send(JSON.stringify(responseObj));
           } else {
             let responseObj = {
